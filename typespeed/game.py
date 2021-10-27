7d55bbcf31011e07ca5cf5b8583ee9935012be47
bauti <jnemi@fie.undef.edu.ar>
2021-10-27 07:44:50 -0300

    ADD - Some declarations in module game

--- typespeed/game.py
+++ typespeed/game.py
@@ -1,7 +1,18 @@
 
 
+def play(player, mode):
+    """ Handles the logic behind each player's turn
+    :param player: dictionary with player information
+    :param mode: string containing game mode
+    """
+
+
 def start(config):
     """ Starts a game with the specified configuration
     :param config: dictionary with game configuration
     """
     print("Started game " + config['mode'])
+    mode = config['mode']
+    for player in config['players']:
+        play(player, mode)
+    # EVALUATE WINNER
