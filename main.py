3688d0e346cedb000703946442ee79dfbe94e21f
bauti <jnemi@fie.undef.edu.ar>
2021-11-06 15:18:45 -0300

    ADD - .txt files with words

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 import typespeed.menu
 
 game = typespeed.menu.start()
-
-rules = {'easy': {'time': 5, 'errors': 5, 'case_insensitive': True},
-             'normal': {'time': 5, 'errors': 3, 'case_insensitive': True},
-             'hard': {'time': 5, 'errors': 3, 'case_insensitive': False}}
