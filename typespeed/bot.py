cb59096266a70ec851e07e4ff0ea58c60bc3c9fd
bauti <jnemi@fie.undef.edu.ar>
2021-11-06 12:15:53 -0300

    MOD - return of bot.simulate(.)

--- typespeed/bot.py
+++ typespeed/bot.py
@@ -6,19 +6,21 @@ def simulate(word, accuracy):
     """ Handles the logic behind the bots
     :param word: string to be typed by the bot
     :param accuracy: float indicating the bot's accuracy
+    :return: word written with or without mistake
     """
     rnd = random.uniform(0, 1)
     if accuracy > rnd:
-        # correct
+        # simulate correct input
         for c in word:
             print(c, end="", flush=True)
             time.sleep(0.2)
         print("")
     else:
-        # incorrect
+        # simulate incorrect input
         mistake = random.randint(len(word)-1)
         word = word[:mistake-1] + word[mistake:]
         for c in word:
             print(c, end="", flush=True)
             time.sleep(0.2)
         print("")
+    return word
