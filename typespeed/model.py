2416e74f72f139dc188d8f57c5133c6f809419cd
bauti <jnemi@fie.undef.edu.ar>
2021-11-11 11:12:20 -0300

    ADD - standby status

--- typespeed/model.py
+++ typespeed/model.py
@@ -2,10 +2,15 @@ from context import context
 from enum import Enum
 
 
-Status = Enum('Status', 'active pause inactive')
+Status = Enum('Status', 'active playing standby pause inactive')
 
 
 def initiate():
     """ Initiates the context of model
     """
     context.model.setdefault('status', Status.active)
+
+
+def pause():
+    """ Pauses the game and calls the pause menu function
+    """
